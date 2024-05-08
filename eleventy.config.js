4f428c893b21d6262a66a7b59dad7e875987a1ab
Andrew Bangs <andrew@bangsphoto.com>
2024-05-08 07:57:24 -0700

    Inline js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,21 @@
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 const CleanCSS = require("clean-css");
+const { minify } = require("terser");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ "static/": "/" });
     eleventyConfig.addWatchTarget("static/**/*.{svg,webp,png,jpg}");
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
     eleventyConfig.addFilter("cssmin", (code) => new CleanCSS({}).minify(code).styles);
+    eleventyConfig.addNunjucksAsyncFilter("jsmin", async (code, cb) => {
+        try {
+            const minified = await minify(code);
+            cb(null, minified.code);
+        } catch (err) {
+            console.error("Terser error: ", err);
+            cb(null, code);
+        }
+    });
 
     return {
         dir: {
