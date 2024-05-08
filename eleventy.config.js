0a88de36801071abb14b6a385577e2dd1064877f
Andrew Bangs <andrew@bangsphoto.com>
2024-05-08 07:40:13 -0700

    Inline css

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,11 @@
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ "static/": "/" });
     eleventyConfig.addWatchTarget("static/**/*.{svg,webp,png,jpg}");
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+    eleventyConfig.addFilter("cssmin", (code) => new CleanCSS({}).minify(code).styles);
 
     return {
         dir: {
